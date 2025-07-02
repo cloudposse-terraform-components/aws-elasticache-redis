fae3a03f5ead33084a1b81f6ce4ea956c7b9ebd2
RoseSecurity <michael@rosesecurity.dev>
2025-07-02 08:51:51 -0400

    fix: update redis engine version

--- test/component_test.go
+++ test/component_test.go
@@ -30,7 +30,7 @@ func (s *ComponentSuite) TestBasic() {
 				"replicas_per_shard": 1,
 				"num_replicas":       1,
 				"engine":             engineName,
-				"engine_version":     "6.2",
+				"engine_version":     "7.0",
 				"instance_type":      "cache.t4g.small",
 				"parameters": []map[string]any{
 					{
