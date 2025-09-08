1b2b457974ce102ebb051eb21a69e9b34dc41c61
RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>
2025-09-08 13:42:12 -0400

    chore: add tests (#31)
    * chore: add tests
    
    * fix: remove dns-primary
    
    * Delete test/run.sh
    
    * Delete test/test_suite.yaml
    
    * fix: add gitignore
    
    * fix: use redis family instead of valkey
    
    * fix: adjust redis version to 6.2
    
    * fix: update redis engine version
    
    * fix: add parameter cluster-allow-pubsubshard-when-down
    
    * fix: destroy with inputs
    
    * fix: update redis family for disabled
    
    * fix: add required fields to disabled
    
    * fix: update deprecated egress arguments
    
    ---------
    
    Co-authored-by: Igor Rodionov <goruha@gmail.com>

---
+++ test/component_test.go
@@ -0,0 +1,76 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/assert"
+)
+
+type ComponentSuite struct {
+	helper.TestSuite
+}
+
+func (s *ComponentSuite) TestBasic() {
+	const component = "elasticache-redis/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	engineName := "redis"
+	uniqueSuffix := strings.ToLower(random.UniqueId())
+
+	inputs := map[string]any{
+		"name": fmt.Sprintf("%s-%s", engineName, uniqueSuffix),
+		"redis_clusters": map[string]any{
+			"redis-test": map[string]any{
+				"num_shards":         0,
+				"replicas_per_shard": 1,
+				"num_replicas":       1,
+				"engine":             engineName,
+				"engine_version":     "7.0",
+				"instance_type":      "cache.t4g.small",
+				"parameters": []map[string]any{
+					{
+						"name":  "cluster-allow-pubsubshard-when-down",
+						"value": "yes",
+					},
+				},
+			},
+		},
+	}
+
+	defer s.DestroyAtmosComponent(s.T(), component, stack, &inputs)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, &inputs)
+	assert.NotNil(s.T(), options)
+
+	s.DriftTest(component, stack, &inputs)
+}
+
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "elasticache-redis/disabled"
+	const stack = "default-test"
+	s.VerifyEnabledFlag(component, stack, nil)
+}
+
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+
+	suite.AddDependency(t, "vpc", "default-test", nil)
+
+	subdomain := strings.ToLower(random.UniqueId())
+	inputs := map[string]any{
+		"zone_config": []map[string]any{
+			{
+				"subdomain": subdomain,
+				"zone_name": "components.cptest.test-automation.app",
+			},
+		},
+	}
+
+	suite.AddDependency(t, "dns-delegated", "default-test", &inputs)
+
+	helper.Run(t, suite)
+}
