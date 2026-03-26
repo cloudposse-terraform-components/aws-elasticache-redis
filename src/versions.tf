da20c497012a8e4832cc019ff4f904548ac38653
RoseSecurity <michael@rosesecurity.dev>
2026-03-26 16:05:30 -0400

    chore: pre-commit fixes

--- src/versions.tf
+++ src/versions.tf
@@ -6,9 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.73.0, < 7.0.0"
     }
-    utils = {
-      source  = "cloudposse/utils"
-      version = ">= 2.0.0, < 3.0.0"
-    }
   }
 }
